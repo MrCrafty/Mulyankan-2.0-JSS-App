4011f78a46c767da4a0eed339a8955a343a7ec19
Jatin Prajapati <jprajapati@horizontal.com>
2023-10-28 09:00:27 +0000

    added tailwind

---
+++ swag-store/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+  theme: {},
+};
